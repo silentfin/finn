77ee419bea011499ef7eee282c9d92c84856b900
silentfin <152957041+silentfin@users.noreply.github.com>
2026-06-06 22:32:09 +0530

    add generate_pages_recursive function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
-from website import copy_files, generate_page
+from website import copy_files, generate_pages_recursive
 
 
 def main():
     copy_files("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 
 if __name__ == "__main__":
--- src/website.py
+++ src/website.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import pathlib
 import shutil
 
 from blocks import markdown_to_html_node
@@ -47,3 +48,16 @@ def generate_page(from_path, template_path, dest_path):
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as t:
         t.write(template)
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for content in os.listdir(dir_path_content):
+        source_path = os.path.join(dir_path_content, content)
+        destination_path = os.path.join(dest_dir_path, content)
+        if os.path.isfile(f"{dir_path_content}/{content}"):
+            destination_path = str(pathlib.Path(destination_path)).replace(
+                ".md", ".html"
+            )
+            generate_page(source_path, template_path, destination_path)
+        else:
+            generate_pages_recursive(source_path, template_path, destination_path)
